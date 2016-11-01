0ae0e7720d15bc920f5ae32eba68b4a2d0a51ed1
wangyunpemg <wangyunpeng@snailgame.net>
2016-11-01 15:00:55 +0800

    first commit

---
+++ commontopbarlibrary/src/main/java/party/loveit/commontopbar/CommonTopBarClick.java
@@ -0,0 +1,10 @@
+package party.loveit.commontopbar;
+
+/**
+ * Created by wangyunpeng on 2016/3/11.
+ */
+public interface CommonTopBarClick {
+    void onClickLeft();
+
+    void onClickRight();
+}
